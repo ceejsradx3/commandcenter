7e802a64de289f33e513faba102efdee0dc33d37
Charles Vincent <cjmvincent@outlook.com>
2021-11-08 13:21:28 -0500

    fix error with loop

--- features.ps1
+++ features.ps1
@@ -4,32 +4,36 @@
 Write-Host "Installing Windows Features..."
 
 # .net Framework
-Enable-WindowsOptionalFeature -Online -All -FeatureName `
-    "NetFx3", `
-    "NetFx4-AdvSrvs", `
-    -NoRestart | Out-Null
+
+$NET = @(
+"NetFx3"
+"NetFx4-AdvSrvs"
+)
+
+foreach ($feature in $NET) {
+Write-Host "Installing $feature"
+Enable-WindowsOptionalFeature -Online -All -FeatureName $NET
+}
 
 # RSAT
-
-Add-WindowsCapability -Online -Name `
-    "Rsat.ActiveDirectory.DS-LDS.Tools~~~~0.0.1.0", `
-    "Rsat.DHCP.Tools~~~~0.0.1.0", `
-    "Rsat.GroupPolicy.Management.Tools~~~~0.0.1.0", `
-    "Rsat.BitLocker.Recovery.Tools~~~~0.0.1.0", `
-    "Rsat.CertificateServices.Tools~~~~0.0.1.0", `
-    "Rsat.Dns.Tools~~~~0.0.1.0", `
-    "Rsat.FailoverCluster.Management.Tools~~~~0.0.1.0", `
-    "Rsat.FileServices.Tools~~~~0.0.1.0", `
-    "Rsat.IPAM.Client.Tools~~~~0.0.1.0", `
-    "Rsat.LLDP.Tools~~~~0.0.1.0", `
-    "Rsat.NetworkController.Tools~~~~0.0.1.0", `
-    "Rsat.NetworkLoadBalancing.Tools~~~~0.0.1.0", `
-    "Rsat.RemoteAccess.Management.Tools~~~~0.0.1.0", `
-    "Rsat.ServerManager.Tools~~~~0.0.1.0" `
-    "Rsat.Shielded.VM.Tools~~~~0.0.1.0" `
-    "Rsat.StorageMigrationService.Management.Tools~~~~0.0.1.0" `
-    "Rsat.StorageReplica.Tools~~~~0.0.1.0" `
-    "Rsat.SystemInsights.Management.Tools~~~~0.0.1.0" `
-    "Rsat.VolumeActivation.Tools~~~~0.0.1.0" `
-    "Rsat.WSUS.Tools~~~~0.0.1.0" `
-    -NoRestart | Out-Null
+# wouldnt work via loop ??!?
+Add-WindowsCapability -Online -Name "Rsat.ActiveDirectory.DS-LDS.Tools~~~~0.0.1.0"
+Add-WindowsCapability -Online -Name "Rsat.DHCP.Tools~~~~0.0.1.0"
+Add-WindowsCapability -Online -Name "Rsat.GroupPolicy.Management.Tools~~~~0.0.1.0"
+Add-WindowsCapability -Online -Name "Rsat.BitLocker.Recovery.Tools~~~~0.0.1.0"
+Add-WindowsCapability -Online -Name "Rsat.CertificateServices.Tools~~~~0.0.1.0"
+Add-WindowsCapability -Online -Name "Rsat.Dns.Tools~~~~0.0.1.0"
+Add-WindowsCapability -Online -Name "Rsat.FailoverCluster.Management.Tools~~~~0.0.1.0"
+Add-WindowsCapability -Online -Name "Rsat.FileServices.Tools~~~~0.0.1.0"
+Add-WindowsCapability -Online -Name "Rsat.IPAM.Client.Tools~~~~0.0.1.0"
+Add-WindowsCapability -Online -Name "Rsat.LLDP.Tools~~~~0.0.1.0"
+Add-WindowsCapability -Online -Name "Rsat.NetworkController.Tools~~~~0.0.1.0"
+Add-WindowsCapability -Online -Name "Rsat.NetworkLoadBalancing.Tools~~~~0.0.1.0"
+Add-WindowsCapability -Online -Name "Rsat.RemoteAccess.Management.Tools~~~~0.0.1.0"
+Add-WindowsCapability -Online -Name "Rsat.ServerManager.Tools~~~~0.0.1.0"
+Add-WindowsCapability -Online -Name "Rsat.Shielded.VM.Tools~~~~0.0.1.0"
+Add-WindowsCapability -Online -Name "Rsat.StorageMigrationService.Management.Tools~~~~0.0.1.0"
+Add-WindowsCapability -Online -Name "Rsat.StorageReplica.Tools~~~~0.0.1.0"
+Add-WindowsCapability -Online -Name "Rsat.SystemInsights.Management.Tools~~~~0.0.1.0"
+Add-WindowsCapability -Online -Name "Rsat.VolumeActivation.Tools~~~~0.0.1.0"
+Add-WindowsCapability -Online -Name "Rsat.WSUS.Tools~~~~0.0.1.0"
